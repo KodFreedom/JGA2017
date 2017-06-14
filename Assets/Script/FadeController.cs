f35e7af31f9ad2245335bf7d9c2829835b49c0c2
KodFreedom <kodfreedom@gmail.com>
2017-06-15 02:53:07 +0900

    Change Fade Mode

--- Assets/Script/FadeController.cs
+++ Assets/Script/FadeController.cs
@@ -6,7 +6,10 @@ using UnityEngine.UI;
 
 public class FadeController : MonoBehaviour
 {
-    public Image m_fadeImg;
+    //public Image m_fadeImg;
+    public Image m_fadeImgL;
+    public Image m_fadeImgR;
+    public float m_fFadeMoveSpeed = 25.0f;
 
     private enum FADE_STATUS
     {
@@ -18,19 +21,26 @@ public class FadeController : MonoBehaviour
     private int m_nIdxNextScene;
     private bool m_bCanChange;
     private FADE_STATUS m_status;
-    private float m_fAlpha;
-    private float m_fFadeRate = 1.0f / 60.0f;
+    //private float m_fAlpha;
+    //private float m_fFadeRate = 1.0f / 60.0f;
+    
 
     public void LoadStage(int nIndex)
     {
         Time.timeScale = 0;
-        m_fadeImg.gameObject.SetActive(true);
+        //m_fadeImg.gameObject.SetActive(true);
+        m_fadeImgL.gameObject.SetActive(true);
+        m_fadeImgR.gameObject.SetActive(true);
         gameObject.SetActive(true);
         m_nIdxNextScene = nIndex;
 
         if(m_status == FADE_STATUS.FADE_OUT) { return; }
-        m_fAlpha = 0.0f;
-        m_fadeImg.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        //m_fAlpha = 0.0f;
+        //m_fadeImg.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+        m_fadeImgL.rectTransform.sizeDelta = new Vector2(Screen.width * 2.0f, Screen.height * 3.0f);
+        m_fadeImgR.rectTransform.sizeDelta = new Vector2(Screen.width * 2.0f, Screen.height * 3.0f);
+        m_fadeImgL.rectTransform.anchoredPosition = new Vector2(-Screen.width * 3.0f, 0.0f);
+        m_fadeImgR.rectTransform.anchoredPosition = new Vector2(Screen.width * 3.0f, 0.0f);
         m_bCanChange = false;
         m_status = FADE_STATUS.FADE_OUT;
     }
@@ -38,9 +48,15 @@ public class FadeController : MonoBehaviour
     private void Start()
     {
         m_bCanChange = false;
-        m_fadeImg.gameObject.SetActive(true);
-        m_fAlpha = 1.0f;
-        m_fadeImg.color = new Color(0.0f, 0.0f, 0.0f, m_fAlpha);
+        //m_fadeImg.gameObject.SetActive(true);
+        //m_fAlpha = 1.0f;
+        //m_fadeImg.color = new Color(0.0f, 0.0f, 0.0f, m_fAlpha);
+        m_fadeImgL.gameObject.SetActive(true);
+        m_fadeImgR.gameObject.SetActive(true);
+        m_fadeImgL.rectTransform.sizeDelta = new Vector2(Screen.width * 2.0f, Screen.height * 3.0f);
+        m_fadeImgR.rectTransform.sizeDelta = new Vector2(Screen.width * 2.0f, Screen.height * 3.0f);
+        m_fadeImgL.rectTransform.anchoredPosition = new Vector2(-Screen.width * 0.5f, 0.0f);
+        m_fadeImgR.rectTransform.anchoredPosition = new Vector2(Screen.width * 0.5f, 0.0f);
         m_status = FADE_STATUS.FADE_IN;
     }
 
@@ -57,24 +73,48 @@ public class FadeController : MonoBehaviour
             case FADE_STATUS.NONE:
                 return;
             case FADE_STATUS.FADE_IN:
-                m_fadeImg.color = new Color(0.0f, 0.0f, 0.0f, m_fAlpha);
-                m_fAlpha -= m_fFadeRate;
-                if (m_fAlpha <= 0.0f)
+                //m_fadeImg.color = new Color(0.0f, 0.0f, 0.0f, m_fAlpha);
+                //m_fAlpha -= m_fFadeRate;
+                //if (m_fAlpha <= 0.0f)
+                //{
+                //    m_fAlpha = 0.0f;
+                //    m_status = FADE_STATUS.NONE;
+                //}
+            {
+                Vector2 vSpeed = new Vector2(m_fFadeMoveSpeed, 0.0f);
+                m_fadeImgL.rectTransform.anchoredPosition -= vSpeed;
+                m_fadeImgR.rectTransform.anchoredPosition += vSpeed;
+
+                if (m_fadeImgL.rectTransform.anchoredPosition.x <= -Screen.width * 3.0f
+                    || m_fadeImgR.rectTransform.anchoredPosition.x >= Screen.width * 3.0f)
                 {
-                    m_fAlpha = 0.0f;
                     m_status = FADE_STATUS.NONE;
                 }
                 break;
+            }
+                
             case FADE_STATUS.FADE_OUT:
-                m_fAlpha += m_fFadeRate;
-                if (m_fAlpha >= 1.0f)
+                //m_fAlpha += m_fFadeRate;
+                //if (m_fAlpha >= 1.0f)
+                //{
+                //    m_fAlpha = 1.0f;
+                //    m_bCanChange = true;
+                //    m_status = FADE_STATUS.NONE;
+                //}
+                //m_fadeImg.color = new Color(0.0f, 0.0f, 0.0f, m_fAlpha);
+            {
+                Vector2 vSpeed = new Vector2(m_fFadeMoveSpeed, 0.0f);
+                m_fadeImgL.rectTransform.anchoredPosition += vSpeed;
+                m_fadeImgR.rectTransform.anchoredPosition -= vSpeed;
+                if (m_fadeImgL.rectTransform.anchoredPosition.x >= -Screen.width * 0.5f
+                    || m_fadeImgR.rectTransform.anchoredPosition.x <= Screen.width * 0.5f)
                 {
-                    m_fAlpha = 1.0f;
-                    m_bCanChange = true;
                     m_status = FADE_STATUS.NONE;
+                    m_bCanChange = true;
                 }
-                m_fadeImg.color = new Color(0.0f, 0.0f, 0.0f, m_fAlpha);
                 break;
+            }
+
             default:
                 break;
         }
