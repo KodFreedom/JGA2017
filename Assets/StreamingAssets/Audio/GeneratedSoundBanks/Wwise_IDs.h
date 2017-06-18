9541496ed7a0744b7dcfd30c3567381abe595bea
KodFreedom <kodfreedom@gmail.com>
2017-06-19 03:53:50 +0900

    Update2017061902

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -13,7 +13,25 @@ namespace AK
 {
     namespace EVENTS
     {
+        static const AkUniqueID BGM_PLAY_START = 604937079U;
+        static const AkUniqueID BGM_PLAY_STOP = 3638082781U;
+        static const AkUniqueID BGM_STAGESELECT_START = 201470811U;
+        static const AkUniqueID BGM_STAGESELECT_STOP = 3163646265U;
+        static const AkUniqueID BGM_TITLE_START = 3000715451U;
+        static const AkUniqueID BGM_TITLE_STOP = 3107979545U;
+        static const AkUniqueID CUBE_HIT_WALL = 3127392263U;
+        static const AkUniqueID FALL_PAUSE = 637465657U;
+        static const AkUniqueID FALL_REPLAY = 2289711140U;
+        static const AkUniqueID FALL_START = 551694363U;
+        static const AkUniqueID FALL_STOP = 1920253945U;
+        static const AkUniqueID GAME_CLEAR = 2825307669U;
         static const AkUniqueID JUMP = 3833651337U;
+        static const AkUniqueID LEFT_WALL = 1320961589U;
+        static const AkUniqueID LR_WALL = 538056262U;
+        static const AkUniqueID PAUSE_SE = 3604790412U;
+        static const AkUniqueID RIGHT_WALL = 3730749304U;
+        static const AkUniqueID SOUND_PAUSE = 332328737U;
+        static const AkUniqueID SOUND_REPLAY = 1922221436U;
     } // namespace EVENTS
 
     namespace BANKS
